7ef760b72e55b82b2fe68fe28ecce29702f98dc6
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2026-03-16 15:30:32 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { ref, watch } from 'vue';
+  import TheHeader from './components/TheHeader.vue';
+  import BaseButton from './components/BaseButton.vue';
+
+  const email = defineModel('email');
+
+  watch(email, (newEmail) => {
+    if (newEmail == "admin@example.com") {
+      email.value = "";
+    }
+
+  });
+
+  const temperature = ref(25)
+  console.log(temperature.value);
+
+  setInterval(() => {
+    temperature.value += 1
+  }, 1000)
+</script>
+
+<template>
+  <TheHeader />
+  <h1>test</h1>
+  <BaseButton label="warning" type="warning"/>
+  <!-- <input  @input="email=this.value()" :value="email" type="email" placeholder="email..."/> -->
+  <input v-model="email" type="email" placeholder="email..."/>
+  {{  email }}
+  <p>Temperature: {{  temperature }}</p>
+</template>
+
+<style>
+ a {
+  color: tomato;
+ }
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { ref } from 'vue';
+  import BaseButton from './BaseButton.vue';
+
+  const theme = ref('light');
+
+  function switchTheme() {
+    theme.value = theme.value === 'light' ? 'dark' : 'light';
+  }
+</script>
+
+<template>
+  <header>
+    <h1>Title</h1>
+    <a href="#t">test</a>
+    <BaseButton @click="switchTheme" :label="theme" type="info"/>
+  </header>
+</template>
+
+<style scoped>
+  h1 {
+    color: #333;
+  }
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+    background-color: #f5f5f5;
+  }
+</style>
