0d86a112cfbc76797a6c86740de53f528d85a853
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2026-03-23 13:48:47 +0100

    Add defineModel example

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   import BaseInputNumber from './components/BaseInputNumber.vue';
   const temperatureSi = ref(0.1 + 0.2); // Kelvin
 
-  const tempKelvin = defineModel({
+  const tempKelvin = defineModel('kelvin', {
     get: () => {
       return (temperatureSi.value).toFixed(2);
     },
@@ -13,7 +13,7 @@
     }
   });
 
-  const tempCelcius = defineModel({
+  const tempCelcius = defineModel('celcius', {
     get: () => {
       return (temperatureSi.value - 273.15).toFixed(2);
     },
