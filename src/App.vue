320e7b12c8a6f1d97bca653f97fd9daf861bf2eb
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2026-03-23 13:46:55 +0100

    Add defineModel example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <script setup>
-  import { ref, watch, computed } from 'vue';
+  import { ref } from 'vue';
   import TheHeader from './components/TheHeader.vue';
   import BaseInputNumber from './components/BaseInputNumber.vue';
-
-  // const email = defineModel('email');
-
-  // watch(email, (newEmail) => {
-  //   if (newEmail == "admin@example.com") {
-  //     email.value = "";
-  //   }
-
-  // });
-
   const temperatureSi = ref(0.1 + 0.2); // Kelvin
 
   const tempKelvin = defineModel({
@@ -23,21 +13,14 @@
     }
   });
 
-  setTimeout(() => {
-    tempKelvin.value = 200;
-  }, 3000)
-
-  const tempCelcius = computed(() => {
-    return (temperatureSi.value - 273.15).toFixed(2);
-  })
-
-
-
-  // console.log(temperature.value);
-
-  // setInterval(() => {
-  //   temperature.value += 1
-  // }, 1000)
+  const tempCelcius = defineModel({
+    get: () => {
+      return (temperatureSi.value - 273.15).toFixed(2);
+    },
+    set: (val) => {
+      temperatureSi.value = Number(val) + 273.15;
+    }
+  });
 </script>
 
 <template>
